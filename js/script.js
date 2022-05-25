ffb7dc57b31c35abfb2d1f39fa6ef31ea3d49147
Renata <renatatof04@gmail.com>
2022-05-25 19:04:47 +0300

    first commit

---
+++ js/script.js
@@ -0,0 +1,14 @@
+"use strict";
+// let arr = [];
+// arr[0]=prompt('Имя', ''); //str
+// arr[1]=prompt('Фамилия', ''); //str
+// arr[2]=+prompt('Возраст', ''); //number
+// console.log(arr);
+// console.log(typeof(arr));
+let ini = 10,
+    dci = 10;
+console.log(ini++);
+console.log(dci--);
+console.log(ini);
+console.log(dci);
+ 
